72fbc8cc49cc4ca522061cf9d452d15b0c049aa8
Malikovich <md.malikovich@gmail.com>
2019-09-12 21:26:07 +0600

    second activity

---
+++ app/src/main/java/com/example/filecabinet/FileActivity.java
@@ -0,0 +1,27 @@
+package com.example.filecabinet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class FileActivity extends AppCompatActivity {
+
+    EditText file_fname;
+    EditText file_sname;
+    EditText file_gender;
+    EditText file_age;
+    EditText file_specialization;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file);
+
+        file_fname = findViewById(R.id.file_fname);
+        file_sname = findViewById(R.id.file_sname);
+        file_gender = findViewById(R.id.file_gender);
+        file_age = findViewById(R.id.file_age);
+        file_specialization = findViewById(R.id.file_specialization);
+    }
+}
