4984d62071682983fea586ea30915f95909ea357
Malikovich <md.malikovich@gmail.com>
2019-09-12 18:58:28 +0600

    VH

---
+++ app/src/main/java/com/example/filecabinet/FileCabinetViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.filecabinet;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FileCabinetViewHolder extends RecyclerView.ViewHolder {
+    public FileCabinetViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
