5e29e9cfbaeb8a2ca5fe0f904284f443e490c11c
Malikovich <md.malikovich@gmail.com>
2019-09-12 18:59:54 +0600

    Adapter

---
+++ app/src/main/java/com/example/filecabinet/FileCabinetAdapter.java
@@ -0,0 +1,24 @@
+package com.example.filecabinet;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FileCabinetAdapter extends RecyclerView.Adapter<FileCabinetViewHolder> {
+    @NonNull
+    @Override
+    public FileCabinetViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FileCabinetViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
